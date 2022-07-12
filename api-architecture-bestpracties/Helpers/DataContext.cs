35014f78f82d139b4d4ee6a74bbb41adc971fcd7
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-11 21:06:31 -0300

    Add method of convertion of typeUser to string in Helpers/DataContext

--- api-architecture-bestpracties/Helpers/DataContext.cs
+++ api-architecture-bestpracties/Helpers/DataContext.cs
@@ -1,5 +1,7 @@
-﻿using api_architecture_bestpracties.Models;
+﻿using api_architecture_bestpracties.Enuns;
+using api_architecture_bestpracties.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace api_architecture_bestpracties.Helpers
 {
@@ -9,6 +11,18 @@ namespace api_architecture_bestpracties.Helpers
         {
 
         }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<UserModel>()
+                .Property(e => e.TypeUser)
+                .HasConversion(
+                v => v.ToString(),
+                v => (TypeUser)Enum.Parse(typeof(TypeUser), v));
+        }
+
 
         public DbSet<UserModel> Users { get; set; }
 
