2adc21e94be9d4dd0ad21be62222920c1bb73e36
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-16 02:38:32 -0300

    Starting UserControllerTest class

--- api-architecture-bestpracties/Tests/Application/Controller/UserControllerTest.cs
+++ api-architecture-bestpracties/Tests/Application/Controller/UserControllerTest.cs
@@ -1,5 +1,8 @@
 ﻿using Xunit;
-
+using Moq;
+using Tweetinvi.Core.Controllers;
+using api_architecture_bestpracties.Controllers;
+using api_architecture_bestpracties.Facades;
 
 namespace api_architecture_bestpracties.Tests.Application.Controller
 
@@ -7,6 +10,16 @@ namespace api_architecture_bestpracties.Tests.Application.Controller
     public class UserControllerTest
     {
 
-       
+        private readonly UserController _userController;
+        private readonly Mock<IUserFacade> _userfacade;
+
+        public UserControllerTest(UserController userController, Mock<IUserFacade> userfacade)
+        {
+            _userfacade = new Mock<IUserFacade>();
+            _userController = new UserController(_userfacade.Object);
+        }
+
+
+
     }
 }
